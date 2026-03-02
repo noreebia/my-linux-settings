ada0048fcddf225d74333a4df3f7cd530812acf1
noreebia <noreebia@gmail.com>
2026-03-03 00:18:15 +0900

    Add installation script for Zsh and related plugins

---
+++ install_zsh.sh
@@ -0,0 +1,7 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install zsh -y
+chsh -s /usr/bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt-get install fonts-powerline
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
