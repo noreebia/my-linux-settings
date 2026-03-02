432c766432e37d97fe63e9fcefec2990660bd4d8
noreebia <noreebia@gmail.com>
2026-03-03 00:25:52 +0900

    Refactor .zshrc modifications to include aliases and plugins setup

--- install_zsh.sh
+++ install_zsh.sh
@@ -6,14 +6,16 @@ sudo apt-get install fonts-powerline
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# modify .zshrc - change the plugins line to the following plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+sed -i 's/^plugins=(.*)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 
-# modify .zshrc - add the following
-# alias upd="sudo apt update && apt upgrade -y"
-# alias cleanup="sudo apt autoremove -y && sudo apt autoclean && sudo apt clean"
-# alias cue="cleanup && upd && exit"
-# alias uc="upd && cleanup"
-# alias uce="upd && cleanup && exit"
-# alias git=hub
-# alias cleanup-branches='git fetch -p && for branch in $(git for-each-ref --format "%(refname) %(upstream:track)" refs/heads | awk '\''$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'\''); do git branch -D $branch; done'
-# alias ll="ls -al"
+cat >> ~/.zshrc << 'EOF'
+
+alias upd="sudo apt update && apt upgrade -y"
+alias cleanup="sudo apt autoremove -y && sudo apt autoclean && sudo apt clean"
+alias cue="cleanup && upd && exit"
+alias uc="upd && cleanup"
+alias uce="upd && cleanup && exit"
+alias git=hub
+alias cleanup-branches='git fetch -p && for branch in $(git for-each-ref --format "%(refname) %(upstream:track)" refs/heads | awk '\''$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'\''); do git branch -D $branch; done'
+alias ll="ls -al"
+EOF
