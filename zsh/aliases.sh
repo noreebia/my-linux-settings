245a5ee566ac0dc3c94048babe45330142a451d0
noreebia <suekimdev@gmail.com>
2026-03-03 14:16:32 +0900

    Add aliases and cdc function to streamline command usage in zsh

---
+++ zsh/aliases.sh
@@ -0,0 +1,19 @@
+
+alias upd="sudo apt update && sudo apt upgrade -y"
+alias cleanup="sudo apt autoremove -y && sudo apt autoclean && sudo apt clean"
+alias cue="cleanup && upd && exit"
+alias uc="upd && cleanup"
+alias uce="upd && cleanup && exit"
+alias ll="ls -al"
+
+alias git-prune-local="git fetch -p && git branch -vv | awk '/: gone] / {print \$1}' | xargs -r git branch -D"
+alias git-prune-local-dry="git branch -vv | awk '/: gone] / {print \$1}'"
+alias gpl="git-prune-local"
+alias gpld="git-prune-local-dry"\
+
+alias szsh="source ~/.zshrc"
+
+cdc() {
+  cd ~/code 2>/dev/null || cd ~/Code 2>/dev/null || echo "Neither directory exists!"
+}
+
