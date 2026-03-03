289f93c6bf101a5187c61569cd934bef494751e8
noreebia <noreebia@gmail.com>
2026-03-04 00:30:55 +0900

    Update installation script for Mise to include activation message and bash instructions

--- prioritzy_10_install_mise.sh
+++ prioritzy_10_install_mise.sh
@@ -5,3 +5,4 @@ curl https://mise.run | sh
 echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
 # or if you don't use zsh
 # echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+echo "Mise has been activated! Run 'source ~/.zshrc' or start a new terminal to apply changes."
