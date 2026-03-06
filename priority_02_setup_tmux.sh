3d3cc9130c5da97949e0449d050535ed7ee84b36
noreebia <noreebia@gmail.com>
2026-03-06 21:41:56 +0900

    Add tmux setup script and configuration file

---
+++ priority_02_setup_tmux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+if ! command -v tmux &> /dev/null; then
+    sudo apt install tmux -y
+fi
+
+cp ./tmux/tmux.conf /etc/tmux.conf
+
+echo "Successfully moved tmux.conf to /etc/tmux.conf. You can keep it there or copy it to ~/.tmux.conf for user-specific settings."
--- priority_10_install_mise.sh
+++ priority_10_install_mise.sh
@@ -3,6 +3,6 @@ set -e
 
 curl https://mise.run | sh
 echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
-# or if you don't use zsh
+# or if I use bash instead of zsh, I can run this line instead:
 # echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
 echo "Mise has been activated! Run 'source ~/.zshrc' or start a new terminal to apply changes."
