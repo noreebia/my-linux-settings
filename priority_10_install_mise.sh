aee0880151bc751eee46f7cce66a751d0d0d42cc
noreebia <noreebia@gmail.com>
2026-03-07 02:37:33 +0900

    Make zshrc-modifying code idempotent

--- priority_10_install_mise.sh
+++ priority_10_install_mise.sh
@@ -2,7 +2,7 @@
 set -e
 
 curl https://mise.run | sh
-echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
+grep -qF 'eval "$(~/.local/bin/mise activate zsh)"' ~/.zshrc || echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
 # or if I use bash instead of zsh, I can run this line instead:
 # echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
 echo "Mise has been activated! Run 'source ~/.zshrc' or start a new terminal to apply changes."
