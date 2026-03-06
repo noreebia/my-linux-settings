683df53c5be4b93d53e20e668d26c785c83a4ff5
noreebia <noreebia@gmail.com>
2026-03-07 02:34:49 +0900

    Update tmux setup script and configuration for improved pane navigation

--- priority_02_setup_tmux.sh
+++ priority_02_setup_tmux.sh
@@ -5,6 +5,6 @@ if ! command -v tmux &> /dev/null; then
     sudo apt install tmux -y
 fi
 
-cp ./tmux/tmux.conf /etc/tmux.conf
+sudo cp ./tmux/tmux.conf /etc/tmux.conf
 
 echo "Successfully moved tmux.conf to /etc/tmux.conf. You can keep it there or copy it to ~/.tmux.conf for user-specific settings."
